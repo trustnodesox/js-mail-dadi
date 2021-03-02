20a5e13fb352c5747c491dda3767369376be40f7
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-02 18:25:43 +0100

    inizio ciclo for

---
+++ main.js
@@ -0,0 +1,11 @@
+var email = prompt("Inserisci la tua email!");
+console.log(email);
+
+var whitelist = ("lollodesossi@gmail.com", "pippo@gmail.com", "pluto@gmail.com");
+console.log(whitelist);
+
+var 
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
