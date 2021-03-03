fade576f37d2d71f1e3909d804db2a01ad6b0e0c
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-03 10:45:17 +0100

    ora funziona, manca il bonus

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ var whitelist = ("lollodesossi@gmail.com", "pippo@gmail.com", "pluto@gmail.com")
 var convalida = false;
 
 for (var i = 0; i < whitelist.length; i++){
-  if (whitelist[i] == email){
+  if (whitelist[i] === email){
       convalida = true;
   }
 }
 
-if (convalida = true) {
-  document.getElementById("verifica").innerHTML = "Complimenti, la tua email è registrata!"
-} else {
+if (convalida) {
   document.getElementById("verifica").innerHTML = "Mi dispiace, non puoi accedere a questo sito!"
+} else {
+  document.getElementById("verifica").innerHTML = "Complimenti, la tua email è registrata!"
 }
